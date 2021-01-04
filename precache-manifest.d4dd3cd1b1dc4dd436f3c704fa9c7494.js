aa5255fae7b0322809c84930fbe16d20b0f6010f
Suman Halder <49319652+programmersumanhalder@users.noreply.github.com>
2021-01-04 13:25:34 +0200

    Add files via upload

---
+++ precache-manifest.d4dd3cd1b1dc4dd436f3c704fa9c7494.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2d47291e36c2c3edd7a701e80da3a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee715282c3ca2388ae2",
+    "url": "/static/css/2.dca1991b.chunk.css"
+  },
+  {
+    "revision": "b8e77e7eb4657b0d5d4b",
+    "url": "/static/css/main.b3c638c2.chunk.css"
+  },
+  {
+    "revision": "6ee715282c3ca2388ae2",
+    "url": "/static/js/2.f201fe46.chunk.js"
+  },
+  {
+    "revision": "85d965a067415930481a1b5c455d0527",
+    "url": "/static/js/2.f201fe46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e77e7eb4657b0d5d4b",
+    "url": "/static/js/main.6d1509ee.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "0f42315392f46e7b84119d08949ca303",
+    "url": "/static/media/youtube.0f423153.png"
+  }
+]);
